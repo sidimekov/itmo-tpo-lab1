862db32a9f3554a225604befe83fd6fdc3a8cdaf
sidimekov <dimasidim8582@gmail.com>
2026-03-11 15:27:16 +0300

    fix: factorial + cosSeries exceptions

--- src/main/java/com/github/sidimekov/Main.java
+++ src/main/java/com/github/sidimekov/Main.java
@@ -1,7 +1,15 @@
 package com.github.sidimekov;
 
+import com.github.sidimekov.task1.CosSeries;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // task 1
+        double x = Math.PI / 3;
+        int n = 3;
+        double resultCos = CosSeries.calculate(x, n);
+        System.out.printf("cos(%.2f), n = %d: %.4f", x, n, resultCos);
     }
 }
--- src/main/java/com/github/sidimekov/task1/CosSeries.java
+++ src/main/java/com/github/sidimekov/task1/CosSeries.java
@@ -1,12 +1,18 @@
 package com.github.sidimekov.task1;
 
 public class CosSeries {
-    private static long factorial(int x) {
-        return x == 1 ? 1 : x * factorial(x - 1);
+    private static double factorial(int x) {
+        if (x < 0) throw new IllegalArgumentException("x < 0 нельзя");
+        double result = 1.0;
+        for (int i = 2; i <= x; i++) {
+            result *= i;
+        }
+        return result;
     }
 
     public static double calculate(double x, int n) {
-        double result = 1;
+        if (n <= 0) throw new IllegalArgumentException("n должен быть положительным");
+        double result = 1.0;
         for (int k = 1; k < n; k++) {
             int sign = k % 2 == 0 ? 1 : -1;
             result += sign * Math.pow(x, 2 * k) / factorial(2 * k);
