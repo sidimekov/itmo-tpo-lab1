f47f19a3f476cd44767385c059801ad2f3415c98
leias19 <liza0272297@yandex.ru>
2026-03-11 14:54:22 +0300

    + ещё несколько вариантов

--- src/test/java/task2/BubbleSortTest.java
+++ src/test/java/task2/BubbleSortTest.java
@@ -60,4 +60,47 @@ public class BubbleSortTest {
         int[] arr = null;
         assertThrows(NullPointerException.class, () -> BubbleSort.sort(arr));
     }
+
+    @Test
+    public void testMaxMinInt() {
+        int[] arr = {Integer.MAX_VALUE, 0, -1, 5, -100, Integer.MIN_VALUE, 50, -50, 1000, -1000};
+        BubbleSort.sort(arr);
+        assertArrayEquals(new int[]{Integer.MIN_VALUE, -1000, -100, -50, -1, 0, 5, 50, 1000, Integer.MAX_VALUE}, arr);
+    }
+
+    @Test
+    public void testAlreadySorted10() {
+        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        BubbleSort.sort(arr);
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10}, arr);
+    }
+
+    @Test
+    public void testReverse10() {
+        int[] arr = {10,9,8,7,6,5,4,3,2,1};
+        BubbleSort.sort(arr);
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10}, arr);
+    }
+
+    @Test
+    public void testRandom10() {
+        int[] arr = {3,7,1,10,4,2,9,6,8,5};
+        BubbleSort.sort(arr);
+        assertArrayEquals(new int[]{1,2,3,4,5,6,7,8,9,10}, arr);
+    }
+
+    @Test
+    public void testAllEqualElements10() {
+        int[] arr = {5,5,5,5,5,5,5,5,5,5};
+        BubbleSort.sort(arr);
+        assertArrayEquals(new int[]{5,5,5,5,5,5,5,5,5,5}, arr);
+    }
+
+    @Test
+    public void testNegativeAndZero10() {
+        int[] arr = {0, -1, -3, 2, -5, 4, -2, 1, -4, 3};
+        BubbleSort.sort(arr);
+        assertArrayEquals(new int[]{-5,-4,-3,-2,-1,0,1,2,3,4}, arr);
+    }
+
 }
